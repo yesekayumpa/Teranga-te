8e96e668591aa0b167ef3817d7235502ee205182
moussabd <109081623+JawkAWUME@users.noreply.github.com>
2026-06-29 14:41:51 +0000

    push 14H42

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
-declare module '*.css';
-
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import '';
-
+import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
